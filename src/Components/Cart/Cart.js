c5fa34cdd8a1a6548239316812444c44058ba293
Ali <aliasad.3128@gmail.com>
2022-12-17 22:09:08 +0330

    dynamic open and close sidebar cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,40 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BsBag } from 'react-icons/bs'
 import { AiOutlineClose } from 'react-icons/ai'
 import './Cart.css'
+import productContext from '../../Contexts/ProductContext'
 
 export default function Cart() {
-  return (
-      <aside className='bag-sidebar'> {/* add active class to show bag sidebar */}
-      <h3 className="bag-title">
-          <span>
-              <BsBag />
-              Bag
-          </span>
-          <span>
-              <AiOutlineClose className='close-icon' />
-          </span>
-      </h3>
+    const contextData = useContext(productContext)
 
-      <div className="row bag-wrapper">
-          <div className='col-12 mt-5'>
-              <div className='card py-3 px-3'>
-                  <div className='col-12 text-center'>
-                      <img src="/images/1.jpg" alt="Product Image" className='cart-img-top w-75' />
-                  </div>
-                  <div className='card-body d-flex flex-column justify-content-center align-items-center'>
-                      <p className="card-text">Samsung A51</p>
-                      <p className="price">12000$</p>
-                      <br />
-                      <a href="#" className='btn btn-danger'>Buy</a>
-                      <a href="#" className='btn btn-outline-dark mt-3 text-capitalize'>
-                          More Information
-                      </a>
-                      <p className="number">1</p>
-                  </div>
-              </div>
-          </div>
-      </div>
-      </aside>
-  )
+    return (
+        <aside className={`bag-sidebar ${contextData.isShowCart ? 'active' : ''}`}> {/* add active class to show bag sidebar */}
+            <h3 className="bag-title">
+                <span>
+                    <BsBag />
+                    Bag
+                </span>
+                <span>
+                    <AiOutlineClose className='close-icon' onClick={() => {
+                        contextData.setIsShowCart(false)
+                    }} />
+                </span>
+            </h3>
+
+            <div className="row bag-wrapper">
+                <div className='col-12 mt-5'>
+                    <div className='card py-3 px-3'>
+                        <div className='col-12 text-center'>
+                            <img src="/images/1.jpg" alt="Product Image" className='cart-img-top w-75' />
+                        </div>
+                        <div className='card-body d-flex flex-column justify-content-center align-items-center'>
+                            <p className="card-text">Samsung A51</p>
+                            <p className="price">12000$</p>
+                            <br />
+                            <a href="#" className='btn btn-danger'>Buy</a>
+                            <a href="#" className='btn btn-outline-dark mt-3 text-capitalize'>
+                                More Information
+                            </a>
+                            <p className="number">1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    )
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BsBag } from "react-icons/bs";
+import productContext from "../../Contexts/ProductContext";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const contextData = useContext(productContext)
   return (
     <nav className="navbar navbar-expand-sm py-3 d-flex">
       <div className="container">
         <a href="#" className="navbar-brand">
-          SabzLearn Shop
+          MY Shop
         </a>
 
         <ul className="navbar-nav me-auto ms-3">
@@ -19,7 +21,9 @@ export default function Navbar() {
         </ul>
 
         <div className="bag-box">
-          <a href="#" className="bag">
+          <a href="javascript:void(0)" className="bag" onClick={() => {
+            contextData.setIsShowCart(true)
+          }}>
             <BsBag className="text-white" />
             <span className="bag-products-counter">0</span>
           </a>
